c56b3d609c5d29601e5d4be4b2657299f95a3c8b
Kaleb Mesfin <kalemesfin12@gmail.com>
2019-10-14 12:48:14 +0300

    Getting used to GitHub

---
+++ helloworld/helloworld.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kalmad99/stringutiltrial"
+)
+
+func main() {
+	fmt.Println(stringutiltrial.Reverse("hello "))
+}
---
+++ stringutiltrial/reverse.go
@@ -0,0 +1,10 @@
+package stringutiltrial
+
+func Reverse(s string) string {
+	// r := []rune{s}
+	// for i, j := 0, len(r)-1; len(r-2){
+
+	// }
+	return s + "abetitota69"
+
+}
